bc2d5d971fdd945e8a9819efcd2ff729a9bf2a23
nikhil-kumar490 <927nikhilkumar@gmail.com>
2025-09-01 01:49:31 +0530

    ;;

--- src/src/demo.java
+++ src/src/demo.java
@@ -1,5 +1,12 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("nik");
+        System.out.println("Helo Nikhil");
+        System.out.println("Hello Nikhil");
+        if (args.length >= 2) {
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+        } else {
+            System.out.println("Please provide two arguments!");
+        }
     }
 }
