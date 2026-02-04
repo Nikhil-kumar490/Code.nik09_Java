a7f607b0fcbf688a780c197d528bfb10e090043d
nikhil-kumar490 <927nikhilkumar@gmail.com>
2026-02-04 15:50:17 +0530

    arr

--- src/src/Array.java
+++ src/src/Array.java
@@ -1,3 +1,16 @@
 public class Array {
+    public static void main(String[] args) {
+        int[] array=new int[10];
+        array[3]=43;
+        array[8]=3;
+        array[2]=9;
+        array[9]=99;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+    }
+
 
 }
